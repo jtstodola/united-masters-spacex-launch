2bbbefb2cc421924ecabf5cd5a2bfc3661f36442
Jessica <jtstodola@gmail.com>
2021-02-13 12:43:36 -0600

    Add image and back button to launch details screen

--- app/screens/LaunchDetails/LaunchDetails.tsx
+++ app/screens/LaunchDetails/LaunchDetails.tsx
@@ -1,14 +1,32 @@
+import { RouteProp } from '@react-navigation/native'
+import { StackNavigationProp } from '@react-navigation/stack'
 import React from 'react'
-import {Text, View} from 'react-native'
+import {Dimensions, Image, Pressable, Text, View} from 'react-native'
+import { NavigationParamsList } from '../../types'
 
 interface LaunchProps {
-  navigation: any
+  navigation: StackNavigationProp<NavigationParamsList, 'LaunchDetails'>
+  route: RouteProp<NavigationParamsList, 'LaunchDetails'>
 }
 
-const LaunchDetails: React.FC<LaunchProps> = ({navigation}) => {
+const LaunchDetails: React.FC<LaunchProps> = ({navigation, route}) => {
+  const {params: {launch}} = route
+  const windowSize = Dimensions.get('window')
+
   return (
-    <View>
-      <Text></Text>
+    <View style={{marginTop: 50, justifyContent: 'space-between', height: windowSize.height - 60}}>
+      <View>
+      {launch.links.flickr_images.length > 0 ? 
+        <Image source={{uri: launch.links.flickr_images[0]}} style={{ height: windowSize.width - 48, width: windowSize.width}} />
+        :
+        <View style={{ width: windowSize.width, justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderColor: '#000'}}>
+          <Text style={{textAlign: 'center'}}>No Image</Text>
+        </View>
+      } 
+      </View>
+      <Pressable onPress={() => navigation.goBack()} style={{marginLeft: 24, marginBottom: 24}}>
+        <Text>Back</Text>
+      </Pressable>
     </View>
   )
 }
