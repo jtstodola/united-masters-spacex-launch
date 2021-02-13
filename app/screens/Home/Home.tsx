4a6d98afed2e881d9891a37443fa4859a2d074fa
Jessica <jtstodola@gmail.com>
2021-02-13 11:41:27 -0600

    Add fetch to spacex api

--- app/screens/Home/Home.tsx
+++ app/screens/Home/Home.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text, View } from 'react-native'
 
 const Home: React.FC = () => {
+  const apiUrl = 'https://api.spacexdata.com/v3/launches'
+  const [spaceX, setSpaceX] = useState()
+
+  fetch(apiUrl).then(response => response.text())
+  .then(result => {
+    console.log(result)
+    setSpaceX(result)
+  })
+  .catch(error => console.log('error', error));
+
+
   return (
     <View>
-      <Text>SpaceX Launch Explorer</Text>
+      <Text style={{marginTop: 80}}>SpaceX Launch Explorer</Text>
     </View>
   )
 }
